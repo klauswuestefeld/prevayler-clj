c9db502dcd24466da9bd66e5a028d6468db0ab47
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2015-05-31 22:27:58 -0300

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject prevayler-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject prevayler-clj "0.1.0"
+  :description "Simple, fast, ACID persistence in Clojure."
+  :url "https://github.com/klauswuestefeld/prevayler-clj"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+                   :plugins [[lein-midje "3.1.3"]]}}
+
+
+
+  :repositories [["clojars" { :sign-releases false}]])

206af81e8a01974632c24f3f2d9cc5b660287e35
Tiago <tiagofabre@gmail.com>
2020-03-28 12:41:38 -0300

    Dependencies bumped

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "https://github.com/klauswuestefeld/prevayler-clj"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.taoensso/nippy "2.14.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]
                    :plugins [[lein-midje "3.1.3"]]}}
 
   :repositories [["clojars" { :sign-releases false}]])

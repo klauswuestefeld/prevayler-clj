6b75a12f25c4968dca9834b632b058f438287d1d
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2020-08-29 18:53:11 -0300

    Clojure, Midje and Nippy versions updated

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject prevayler-clj "3.1.0-SNAPSHOT"
+(defproject prevayler-clj "4.0.0-SNAPSHOT"
   :description "Simple, fast, ACID persistence in Clojure."
   :url "https://github.com/klauswuestefeld/prevayler-clj"
   :license {:name "BSD"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.taoensso/nippy "2.13.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.taoensso/nippy "2.15.1"]]
+  :profiles {:dev {:dependencies [[midje "1.9.9"]]
                    :plugins [[lein-midje "3.1.3"]]}}
 
   :repositories [["clojars" {:sign-releases false}]])

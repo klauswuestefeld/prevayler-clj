fa7c88674b72fb0a79425ed165238f3fd18ab3dd
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2020-08-30 17:40:23 -0300

    Timestamps done.

--- src/prevayler4.clj
+++ src/prevayler4.clj
@@ -5,12 +5,16 @@
    [java.io File FileOutputStream FileInputStream DataInputStream DataOutputStream EOFException Closeable]
    [clojure.lang IDeref]))
 
+(defn- rename! [file new-file]
+  (when-not (.renameTo file new-file)
+    (throw (RuntimeException. (str "Unable to rename " file " to " new-file)))))
+
 (defn- produce-backup-file! [file]
   (let [backup (File. (str file ".backup"))]
     (if (.exists backup)
       backup
       (when (.exists file)
-        (assert (.renameTo file backup))
+        (rename! file backup)
         backup))))
 
 (defn- try-to-restore! [handler state-atom data-in]
@@ -18,37 +22,38 @@
     (let [previous-state (read-value!)] ; Can throw EOFException
       (reset! state-atom previous-state))
     (while true ;Ends with EOFException
-      (let [event (read-value!)]
-        (swap! state-atom handler event)))))
+      (let [[timestamp event] (read-value!)]
+        (try
+          (swap! state-atom handler event timestamp)
+          (catch Exception e (.printStackTrace e)))))))
 
 (defn- restore! [handler state-atom ^File file]
   (with-open [data-in (-> file FileInputStream. DataInputStream.)]
     (try
       (try-to-restore! handler state-atom data-in)
-
       (catch EOFException _done)
       (catch Exception corruption
-        (println "Warning - Corruption at end of prevalence file (this is normally OK and can happen when the process is killed during write):" corruption)))))
+        (println "Warning - Exception thrown while reading journal (this is normally OK and can happen when the process is killed during write):" corruption)))))
 
 (defn- write-with-flush! [data-out value]
   (nippy/freeze-to-out! data-out value)
   (.flush data-out))
 
-(defn- archive! [^File file]
-  (let [new-file (File. (str file "-" (System/currentTimeMillis)))]
-    (assert (.renameTo file new-file))))
+(defn- archive! [^File backup]
+  (rename! backup (File. (str backup "-" (System/currentTimeMillis)))))
 
 (defprotocol Prevayler
-  (handle! [_ event] "Handle event and return the new state."))
+  (handle! [_ event] "Journals the event, applies the business function to the state and the event; and returns the new state."))
 
-(defn prevayler! [{:keys [initial-state journal-file handler]
+(defn prevayler! [{:keys [initial-state business-fn timestamp-fn journal-file]
                    :or {initial-state {}
+                        timestamp-fn #(System/currentTimeMillis)
                         journal-file (File. "journal4")}}]
   (let [state-atom (atom initial-state)
         backup (produce-backup-file! journal-file)]
 
     (when backup
-      (restore! handler state-atom backup))
+      (restore! business-fn state-atom backup))
 
     (let [data-out (-> journal-file FileOutputStream. DataOutputStream.)]
       (write-with-flush! data-out @state-atom)
@@ -59,8 +64,9 @@
         Prevayler
         (handle! [this event]
           (locking this ; (I)solation: strict serializability.
-            (let [new-state (handler @state-atom event)] ; (C)onsistency: must be guaranteed by the handler. The event won't be journalled when the handler throws an exception.)
-              (write-with-flush! data-out event) ; (D)urability
+            (let [timestamp (timestamp-fn)
+                  new-state (business-fn @state-atom event timestamp)] ; (C)onsistency: must be guaranteed by the handler. The event won't be journalled when the handler throws an exception.)
+              (write-with-flush! data-out [timestamp event]) ; (D)urability
               (reset! state-atom new-state)))) ; (A)tomicity
 
         IDeref (deref [_] @state-atom)
--- test/prevayler_test.clj
+++ test/prevayler_test.clj
@@ -5,63 +5,78 @@
   (:import
     [java.io File]))
 
-(defn- handler [state event]
+(def initial-state {:contacts []})
+
+(defn- contact-list
+  "Our contact list system function."
+  [state event timestamp]
   (when (= event "boom")
-    (throw (RuntimeException.)))
-  (str state event))
-
-(fact "journal4 is the default file name"
-  (->
-    (prevayler! {:handler handler})
-    (.close))
-  (.delete (File. "journal4")) => true)
-
-(def initial-state "A")
+    (throw (RuntimeException.))) ; Simulate a bug.
+  (-> state
+      (update :contacts conj event)
+      (assoc :last-timestamp timestamp)))
 
 (defn- tmp-file []
   (doto
     (File/createTempFile "test-" ".tmp")
     (.delete)))
 
-(facts "About prevalence"
-  (let [journal (tmp-file)
-        prev! #(prevayler! {:initial-state initial-state
-                            :journal-file journal
-                            :handler handler})]
+(defn counter [start]
+  (let [counter-atom (atom start)]
+    #(swap! counter-atom inc)))
 
+(def t0 1598801265000)  ; System/currentTimeMillis at some arbitrary moment in the past.
+
+(facts "About prevalence"
+  (with-open [p (prevayler! {:initial-state initial-state
+                             :business-fn contact-list})]
+    (fact "The System clock is used as the default timestamp-fn"
+      (handle! p "Ann")
+      (-> @p :last-timestamp (> t0)) => true))
+       
+  (fact "journal4 is the default file name and it is released after Prevayler is closed (Relevant in Windows)."
+    (.delete (File. "journal4")) => true)
+       
+  (let [counter (counter t0)
+        journal (tmp-file)
+        prev! #(prevayler! {:initial-state initial-state
+                            :business-fn contact-list
+                            :timestamp-fn counter ; Timestamps must be controlled while testing.
+                            :journal-file journal})]
+ 
     (fact "First run uses initial state"
       (with-open [p (prev!)]
-        @p => "A"))
+        (:contacts @p) => []))
 
     (fact "Restart after no events recovers initial state"
       (with-open [p (prev!)]
-        @p => "A"
-        (handle! p "B") => "AB"
-        @p => "AB"
-        (handle! p "C") => "ABC"
-        @p => "ABC"))
+        (:contacts @p) => []
+        (handle! p "Ann") => {:contacts ["Ann"]
+                              :last-timestamp 1598801265001}
+        (:contacts @p) => ["Ann"]
+        (handle! p "Bob")
+        (:contacts @p) => ["Ann" "Bob"]))
 
     (fact "Restart after some events recovers last state"
       (with-open [p (prev!)]
-        @p => "ABC"))
+        (:contacts @p) => ["Ann" "Bob"]))
 
     (fact "Simulated crash during restart is survived"
       (.renameTo journal (File. (str journal ".backup"))) => true
       (spit journal "#$@%@corruption&@#$@")
       (with-open [p (prev!)]
-        @p => "ABC"))
+        (:contacts @p) => ["Ann" "Bob"]))
     
     (fact "Simulated crash during event handle will fall through"
       (with-open [p (prev!)]
         (handle! p "boom") => (throws RuntimeException)
-        @p => "ABC"
-        (handle! p "D") => "ABCD"))
+        (:contacts @p) => ["Ann" "Bob"]
+        (handle! p "Cid")
+        (:contacts @p) => ["Ann" "Bob" "Cid"]))
 
     (fact "Restart after some crash during event handle recovers last state"
       (with-open [p (prev!)]
-        @p => "ABCD"))
-
-    (fact "Journal file is released after Prevayler is closed (Relevant in Windows)."
-      (.delete journal) => true)))
+        @p => {:contacts ["Ann" "Bob" "Cid"]
+               :last-timestamp 1598801265004}))))
 
 ; (do (require 'midje.repl) (midje.repl/autotest))

2034045a31be0e0095f3b5dc40be560e2ff4550b
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2020-08-31 13:15:50 -0300

    Version renamed to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prevayler-clj/prevayler4 "2020.08.31"
+(defproject prevayler-clj/prevayler4 "2020.09.01-SNAPSHOT"
   :description "Simple, fast, 100% transparent, ACID persistence in Clojure."
   :url "https://github.com/klauswuestefeld/prevayler-clj"
   :license {:name "BSD"

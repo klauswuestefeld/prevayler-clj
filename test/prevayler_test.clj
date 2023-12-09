8961a2de71a5729405013c8477b6d5cae4811761
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2023-12-09 19:15:54 -0300

    Only journal events that change the state

--- test/prevayler_test.clj
+++ test/prevayler_test.clj
@@ -10,11 +10,12 @@
 (defn- contact-list
   "Our contact list system function."
   [state event timestamp]
-  (when (= event "boom")
-    (throw (RuntimeException.))) ; Simulate a bug.
-  (-> state
-      (update :contacts conj event)
-      (assoc :last-timestamp timestamp)))
+  (case event
+    "do-nothing" state
+    "simulate-a-bug" (throw (RuntimeException.))
+    (-> state
+        (update :contacts conj event)
+        (assoc :last-timestamp timestamp))))
 
 (defn- tmp-file []
   (doto
@@ -65,7 +66,13 @@
     (fact "Restart after some events recovers last state"
       (with-open [p (prev!)]
         (:contacts @p) => ["Ann" "Bob"]))
-
+    
+    (fact "Events that don't change the state are not journalled"
+      (with-open [p (prev!)]
+        (let [previous-length (.length journal)]
+          (:contacts (handle! p "do-nothing")) => ["Ann" "Bob"]
+          (.length journal) => previous-length)))
+    
     (fact "Simulated crash during restart is survived"
       (.renameTo journal (File. (str journal ".backup"))) => true
       (spit journal "#$@%@corruption&@#$@")
@@ -74,7 +81,7 @@
     
     (fact "Exception during event handle doesn't affect state"
       (with-open [p (prev!)]
-        (handle! p "boom") => (throws RuntimeException)
+        (handle! p "simulate-a-bug") => (throws RuntimeException)
         (:contacts @p) => ["Ann" "Bob"]
         (handle! p "Cid")
         (:contacts @p) => ["Ann" "Bob" "Cid"]))
@@ -82,7 +89,7 @@
     (fact "Restart after some crash during event handle recovers last state"
       (with-open [p (prev!)]
         @p => {:contacts ["Ann" "Bob" "Cid"]
-               :last-timestamp 1598800000005}))
+               :last-timestamp 1598800000006}))
     
     (fact "Restart with inconsistent business-fn throws exception"
           (with-open [p (prev!)]

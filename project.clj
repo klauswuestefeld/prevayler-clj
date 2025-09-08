1b8d66b058eac78a5e66c10dabd31be29da69427
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2025-09-07 23:57:12 -0300

    Maven group prevayler-clj -> house.jux

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prevayler-clj/prevayler5 "0.0.1"
+(defproject house.jux/prevayler5 "0.0.1"
   :description "Simple, fast, 100% transparent, ACID persistence in Clojure."
   :url "https://github.com/klauswuestefeld/prevayler-clj"
   :license {:name "BSD"

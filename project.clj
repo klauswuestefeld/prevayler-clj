35f2c57bad83896f9d41326714c448c584df51ac
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2025-09-10 00:56:19 -0300

    Minor cleanup

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject house.jux/prevayler5 "0.0.1"
-  :description "Simple, fast, 100% transparent, ACID persistence in Clojure."
-  :url "https://github.com/klauswuestefeld/prevayler-clj"
-  :license {:name "BSD"
-            :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.11.4"]
-                 [com.taoensso/nippy "3.6.0"]])

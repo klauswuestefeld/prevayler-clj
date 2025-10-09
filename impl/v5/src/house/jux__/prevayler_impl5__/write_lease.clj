a9d38431526254f201c319dba9859835267fa2f6
Rui Fernando Hayashi <rfhayashi@gmail.com>
2025-10-09 16:04:18 -0300

    move comment

--- impl/v5/src/house/jux__/prevayler_impl5__/write_lease.clj
+++ impl/v5/src/house/jux__/prevayler_impl5__/write_lease.clj
@@ -1,3 +1,42 @@
+; TODO: Cooperative Consistency Across Multiple Instances Sharing the Same Network Dir
+; ====================================================================================
+;
+; Init
+;  Loop until successful:
+;    Delete all files inside the "lock" folder, if any.
+;    Delete the "lock" folder, if it exists.
+;    Create "lock" folder. This is important: I must create it, it can't already exist.
+;      Create owner-{uuid}.a file.
+;
+; Wait 1min, so the old owner, if any, can yield.
+; Loop every 30s, to see if I still am the owner: (lease mechanism)
+;   Rename lock/owner-{uuid}.a file to lock/owner-{uuid}.b or vice-versa.
+;     If success: lastSuccessfulLeaseCheck atom = currentTimeMillis; error atom = nil.
+;     If neither file (a nor b) exist: deposed atom = true
+;     If some other error: error atom = error message
+;    
+; Check before every write: Prevayler is free to write if:
+;   Not deposed
+;   AND Not error
+;   AND lastSuccessfulLeaseCheck newer than 40s.
+;
+;
+; TODO: Bonus: Extra Resilience Against Zombie Journal Writes
+; -----------------------------------------------------------
+;
+; Zombie writes are writes arriving late at the server from some old dead owner that were buffered on a temporarily disconnected network client.
+; To ignore zombie journal appends:
+; 
+; For each journal to read:
+;   Does a file called {journal-number}.journal-metadata5 exist?
+;     No: read the whole journal and write this metadata file with the number of events I just read.
+;     Yes: read only as many events as the amount recorded in the metadata file.
+;
+; To avoid late zombie journal file creation from overriding existing journals:
+;   Before creating a new journal file:
+;     Create a lock/{journal-number}.journal-lock directory (MKDIR the only atomic mutually-exclusive operation we can trust. Even file renames will override each other.)
+;       If it already exists, but the journal file does not, treat the same as an empty journal.
+
 (ns house.jux--.prevayler-impl5--.write-lease)
 
 (set! *warn-on-reflection* true)

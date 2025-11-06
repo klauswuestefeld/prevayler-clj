59fe75129bbb90f6feb50c6b18f380f7d0e0d86b
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2025-11-06 16:10:34 -0300

    Concurrency docs

--- impl/v5/src/house/jux__/prevayler_impl5__/storage.clj
+++ impl/v5/src/house/jux__/prevayler_impl5__/storage.clj
@@ -7,14 +7,18 @@
     "Return a map with:
        :snapshot - The state saved in the last snapshot. Defaults to default-state.
        :events - A lazy sequence of all events after that snapshot.
-     Must be called before the first invocation of append-to-journal!")
+     Must be called only once.")
 
   (append-to-journal! [this event]
     "Serializes event (opaque value) and appends it to some reasonably durable journal.
-     Throws on failure.")
+     Throws on failure.
+     Must be called after the latest-journal events have been consumed.
+     Should not be called concurrently with start-taking-snapshot! (caller must synchronize externally) because this Storage does not have information to properly sequence them.")
 
   (start-taking-snapshot! [this state]
     "Serializes state (opaque value) and stores it as a snapshot asynchronously.
-     Returns an IDeref that resolves to :done or throws on error."))
+     Returns an IDeref that resolves to :done or throws on error.
+     Must be called after the latest-journal events have been consumed.
+     Should not be called concurrently with append-to-journal! (caller must synchronize externally) because this Storage does not have information to properly sequence them."))
 
 ; TODO: Be resilient to errors.
